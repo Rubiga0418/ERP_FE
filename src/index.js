2dc236f15cfd69c2fe01a22777241fc4d1a64885
devaraj <kumarbala3557@gmail.com>
2024-10-09 00:26:44 +0530

    modified 1

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import GoogleFontLoader from './GoogleFontLoader';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <GoogleFontLoader />
+    <GoogleFontLoader />
     <FontAwesomeLoader />
     <App />
   </React.StrictMode>
